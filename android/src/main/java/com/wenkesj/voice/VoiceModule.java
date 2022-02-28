40de79269ceb1525fd43f0dd481c479b6fc58361
Fabien Drault <fdrault@betomorrow.com>
2022-02-28 17:45:26 +0100

    fix(android): react-native 0.65 event emitter stubs

--- android/src/main/java/com/wenkesj/voice/VoiceModule.java
+++ android/src/main/java/com/wenkesj/voice/VoiceModule.java
@@ -277,6 +277,16 @@ public class VoiceModule extends ReactContextBaseJavaModule implements Recogniti
       .emit(eventName, params);
   }
 
+   @ReactMethod
+  public void addListener(String eventName) {
+    // Keep: Required for RN built in Event Emitter Calls.
+  }
+
+  @ReactMethod
+  public void removeListeners(Integer count) {
+    // Keep: Required for RN built in Event Emitter Calls.
+  }
+
   @Override
   public void onBeginningOfSpeech() {
     WritableMap event = Arguments.createMap();
